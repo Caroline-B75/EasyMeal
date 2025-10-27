d1111c491fa2cde2451b43cc51a873e6e3f91528
cbelmas <caroline.belmas@biokortex.com>
2025-10-27 16:23:50 +0100

    Setup base env (Rails + PG + Devise + Pundit + AS + RSpec)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError do
+    redirect_to(root_path, alert: "Accès refusé.")
+  end
 end
